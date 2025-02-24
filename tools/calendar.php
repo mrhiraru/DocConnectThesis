5f11b15f4406307c05c2921f4e3ff5897434ea61
mrhiraru <mrhiraru@gmail.com>
2025-02-24 21:51:56 +0800

    2

--- tools/calendar.php
+++ tools/calendar.php
@@ -1,5 +1,6 @@
 <script type="text/javascript">
     const doctorEmail = '<?= $_SESSION['email'] ?>'; //original code
+    /* hostinger auto deployment success */
     /* exported gapiLoaded */
     /* exported gisLoaded */
     /* exported handleAuthClick */
