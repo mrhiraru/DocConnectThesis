a6897ee67f5bffa0a2dc2fac23754a7d0ab49eba
Frankii <franklinoliveros2003@gmail.com>
2025-02-25 22:14:29 +0800

    admin s=user srvice page form

--- admin/includes/adminUserPage_nav.php
+++ admin/includes/adminUserPage_nav.php
@@ -4,7 +4,7 @@
       <ol class="breadcrumb mb-0">
         <li class="breadcrumb-item <?= $userPage ?>" aria-current="<?= $aUserPage ?>"><a href="./userPage" class="<?= $cUserPage ?>">Home</a></li>
         <li class="breadcrumb-item <?= $aboutUs ?>" aria-current="<?= $aAboutUs ?>"><a href="./userAboutUs" class="<?= $cAboutUs ?>">About Us</a></li>
-        <li class="breadcrumb-item <?= $termsOfServices ?>" aria-current="<?= $aTermsOfServices ?>"><a href="./termsOfServices" class="<?= $cTermsOfServices ?>">Services</a></li>
+        <li class="breadcrumb-item <?= $services ?>" aria-current="<?= $aServices ?>"><a href="./userServices" class="<?= $cServices ?>">Services</a></li>
         <li class="breadcrumb-item <?= $aboutUs ?>" aria-current="<?= $aAboutUs ?>"><a href="./settingsAboutUs" class="<?= $cAboutUs ?>">Contact us</a></li>
       </ol>
     </nav>
