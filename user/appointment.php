41e0b367f8e4d23c3a9cd180fa9a0b6a07a2688c
mrhiraru <mrhiraru@gmail.com>
2025-03-01 06:06:11 +0800

    23

--- user/appointment.php
+++ user/appointment.php
@@ -78,9 +78,9 @@ include '../includes/head.php';
     ?>
 
     <div class="row">
-        <section id="appointment" class="col-6 p-3">
+        <section id="appointment" class="col-6 page-container padding-medium p-3">
             <div class="row mb-3">
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-8 col-lg-6">
                     <form id="appointmentForm" action="" method="post" class="border border-dark-subtle shadow-sm rounded-2 p-3 mb-4 mb-md-0">
                         <div class="row">
                             <div class="col-12">
@@ -205,9 +205,9 @@ include '../includes/head.php';
                 </div>
             </div>
         </section>
-        <section id="appointment" class="col-6 p-3">
+        <section id="appointment" class="col-6 page-container padding-medium p-3">
             <div class="row mb-3">
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-8 col-lg-6">
                     <form id="appointmentForm" action="" method="post" class="border border-dark-subtle shadow-sm rounded-2 p-3 mb-4 mb-md-0">
                         <div class="row">
                             <div class="col-12">
