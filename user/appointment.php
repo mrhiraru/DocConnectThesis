125fb2e4fefb6dc624fc81176260680cff43ec50
mrhiraru <mrhiraru@gmail.com>
2025-03-01 09:04:16 +0800

    2

--- user/appointment.php
+++ user/appointment.php
@@ -182,7 +182,7 @@ include '../includes/head.php';
 
                 <hr class="my-2">
                 <div class="w-100 d-flex justify-content-end">
-                    <button id="request" name="request" type="submit" class="col-12 col-md-6 col-lg-4 btn btn-primary mt-2" disabled>Request Appointment</button>
+                    <button id="request" name="request" type="submit" class="col-12 col-md-6 col-lg-4 btn btn-primary text-light mt-2" disabled>Request Appointment</button>
                 </div>
             </div>
         </section>
