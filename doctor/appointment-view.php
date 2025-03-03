b055b43416a51d7d0136eedb5d24190ac6e8192e
mrhiraru <mrhiraru@gmail.com>
2025-03-04 01:40:21 +0800

    2

--- doctor/appointment-view.php
+++ doctor/appointment-view.php
@@ -56,7 +56,7 @@ include '../includes/head.php';
                                 <div class="col-12 mb-3">
                                     Does the patient have a medical condition?
                                     <div class="form-check form-check-inline ms-3">
-                                        <input class="form-check-input" type="radio" name="medcon_check" id="Yes" value="Yes" onclick="show_medical_conditions()">
+                                        <input class="form-check-input" type="radio" name="medcon_check" id="Yes" value="Yes">
                                         <label class="form-check-label" for="Yes">Yes</label>
                                     </div>
                                     <div class="form-check form-check-inline">
@@ -67,7 +67,9 @@ include '../includes/head.php';
                                 <div class="col-12 mb-3">
                                     <label for="medcon" class="form-label">Diagnosis:</label>
                                     <select class="form-select" name="medcon" id="medcon">
-                                        
+                                        <?php 
+                                        include_once('../handlers/appointment-view.fetch_conditions.php');
+                                        ?>  
                                     </select>
                                 </div>
                                 <div class="col-12 mb-3">
