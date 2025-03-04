d0938e41b230e00b070d3240be460836a88a29ab
mrhiraru <mrhiraru@gmail.com>
2025-03-05 05:47:06 +0800

    2

--- user/test-appointment.php
+++ user/test-appointment.php
@@ -191,7 +191,7 @@ include '../includes/head.php';
                         <div class="col-lg-6">
                             <label for="appointment_date" class="form-label text-secondary fw-semibold">Select Date</label>
                             <div class="p-2 border rounded bg-light shadow-sm">
-                                <input type="text" id="appointment_date" name="appointment_date" class="form-control border-0 text-center fs-6 mb-3 border border-dark" placeholder="MONTH DD YYYY" required readonly>
+                                <input type="text" id="appointment_date" name="appointment_date" class="form-control border-0 text-center fs-6 mb-3 border border-dark" placeholder="SELECT DAY" required readonly>
                             </div>
                             <?php if (isset($_POST['appointment_date']) && !validate_field($_POST['appointment_date'])) { ?>
                                 <p class="text-danger small mt-1">Select a valid appointment date.</p>
@@ -202,7 +202,7 @@ include '../includes/head.php';
                         <div class="col-lg-6">
                             <label for="appointment_time" class="form-label text-secondary fw-semibold">Select Time</label>
                             <div class="p-2 pb-3 border rounded bg-light shadow-sm">
-                                <input type="text" id="appointment_time" name="appointment_time" class="form-control border-0 text-center fs-6 mb-3 border border-dark" placeholder="HH:MM AM/PM" required>
+                                <input type="text" id="appointment_time" name="appointment_time" class="form-control border-0 text-center fs-6 mb-3 border border-dark" placeholder="SELECT TIME" required readonly>
                             </div>
                             <?php
                             if (isset($_POST['appointment_time']) && !validate_field($_POST['appointment_time'])) {
