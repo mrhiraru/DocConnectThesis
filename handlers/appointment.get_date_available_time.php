3ce0118d52f6c09bb2d138d0312122ae8a34d546
mrhiraru <mrhiraru@gmail.com>
2025-03-06 13:42:07 +0800

    232

--- handlers/appointment.get_date_available_time.php
+++ handlers/appointment.get_date_available_time.php
@@ -4,8 +4,6 @@ $appointment = new Appointment();
 
 $takenHours = $appointment->get_taken_hours($_GET['doctor_id'], $_GET['date']);
 
-$takenHours = ["01:00:00", "02:00:00", "11:00:00"];
-
 $formattedHours = array_map(function ($time) {
     return (int)explode(":", $time)[0]; // Extract and convert hour to integer
 }, $takenHours);
