9b3722b9dbd93b1b6ec844f685b0665e97f07bcd
Frankii <franklinoliveros2003@gmail.com>
2025-03-06 12:35:55 +0800

    basta yan

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -97,12 +97,10 @@ function getCurrentPage()
     </div>
 
     <div class="container px-0 px-md-2">
-      <div class="row mx-2">
+      <div class="row mx-2 mb-3">
         <div class="col-12 col-lg-8">
           <div class="card h-100">
-            <div class="card-header">
-              <h3 class="text-center">Appoint Management</h3>
-            </div>
+            <div class="card-header fs-4 text-center">Appoint Management</div>
             <div class="p-3">
               <h5>Appointment Overview</h5>
               <ul class="list-group list-group-flush">
@@ -147,39 +145,41 @@ function getCurrentPage()
         </div>
         <div class="col-lg-4">
           <div class="card h-100">
-            <div class="card-header">
-              <h4 class="card-title">Appointment Insights</h4>
-            </div>
+            <div class="card-header fs-4 text-center">Appointment Insights</div>
             <div class="card-body">
               <canvas id="appointmentChart"></canvas>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="col-12 col-lg-8">
-      <nav>
-        <div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
-          <button class="nav-link  active" id="nav-campus-tab" data-bs-toggle="tab" data-bs-target="#nav-campus" type="button" role="tab" aria-controls="nav-campus" aria-selected="true">Campus</button>
-          <button class="nav-link" id="nav-type-tab" data-bs-toggle="tab" data-bs-target="#nav-type" type="button" role="tab" aria-controls="nav-type" aria-selected="false">Type</button>
+      <div class="row mx-2 g-3">
+        <div class="col-12 col-lg-4">
         </div>
-      </nav>
-      <div class="tab-content p-3 border bg-white" id="nav-tabContent">
-        <select id="yearSelect" class="form-select form-select-sm w-25" aria-label=".form-select-sm example">
-          <option value="1">2021-2022</option>
-          <option value="2">2022-2023</option>
-          <option value="3">2023-2024</option>
-        </select>
-
-        <div class="tab-pane fade active show" id="nav-campus" role="tabpanel" aria-labelledby="nav-campus-tab">
-          <canvas id="campusChart" class="chart" role="img"></canvas>
-        </div>
-        <div class="tab-pane fade" id="nav-type" role="tabpanel" aria-labelledby="nav-type-tab">
-          <canvas id="typeChart" class="chart" role="img"></canvas>
+        <div class="col-12 col-lg-8">
+          <div class="card h-100">
+            <div class="card-header">
+              <h4 class="text-center">Appoint Management</h4>
+            </div>
+            <div class="card-cody">
+          
+              <div class="p-3" id="nav-tabContent">
+                <select id="yearSelect" class="form-select form-select-sm w-25" aria-label="form-select-sm example">
+                  <option value="1">2021-2022</option>
+                  <option value="2">2022-2023</option>
+                  <option value="3">2023-2024</option>
+                </select>
+  
+                <div class="tab-pane fade active show" id="nav-campus" role="tabpanel" aria-labelledby="nav-campus-tab">
+                  <canvas id="campusChart" class="chart" role="img"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
   </section>
 
   <script src="./js/analytics-lineChart.js"></script>
--- classes/account.class.php
+++ classes/account.class.php
@@ -616,20 +616,19 @@ class Account
         $totalDoctors = $queryDoctors->fetch(PDO::FETCH_ASSOC)['total'];
 
         // Fetch patient names, doctor names, and appointment dates
-        $sqlAppointments = "SELECT 
-    a2.firstname AS patient_firstname, 
-    a2.lastname AS patient_lastname, 
-    a1.firstname AS doctor_firstname, 
-    a1.lastname AS doctor_lastname, 
-    ap.appointment_date,
-    ap.appointment_time
-FROM appointment ap
-LEFT JOIN doctor_info d ON ap.doctor_id = d.doctor_id
-LEFT JOIN account a1 ON d.account_id = a1.account_id
-LEFT JOIN patient_info p ON ap.patient_id = p.patient_id
-LEFT JOIN account a2 ON p.account_id = a2.account_id
-WHERE DATE(ap.appointment_date) = CURDATE()
-ORDER BY ap.appointment_date DESC, ap.appointment_time ASC";
+        $sqlAppointments = "SELECT a2.firstname AS patient_firstname, 
+                                   a2.lastname AS patient_lastname, 
+                                   a1.firstname AS doctor_firstname, 
+                                   a1.lastname AS doctor_lastname, 
+                                   ap.appointment_date,
+                                   ap.appointment_time
+                            FROM appointment ap
+                            LEFT JOIN doctor_info d ON ap.doctor_id = d.doctor_id
+                            LEFT JOIN account a1 ON d.account_id = a1.account_id
+                            LEFT JOIN patient_info p ON ap.patient_id = p.patient_id
+                            LEFT JOIN account a2 ON p.account_id = a2.account_id
+                            WHERE DATE(ap.appointment_date) = CURDATE()
+                            ORDER BY ap.appointment_date DESC, ap.appointment_time ASC";
 
         $queryAppointments = $this->db->connect()->prepare($sqlAppointments);
         $queryAppointments->execute();
@@ -640,9 +639,11 @@ ORDER BY ap.appointment_date DESC, ap.appointment_time ASC";
             'totalActiveUsers' => $activeUsers,
             'totalPatients' => $totalPatients,
             'totalDoctors' => $totalDoctors,
-            'appointments' => $appointments // Add the appointments data to the return array
+            'appointments' => $appointments
         ];
     }
+
+    
 
 
     // ---APPOINMENTS FUNCTIONS START---
