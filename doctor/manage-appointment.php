8381387c81c74bc4db6e37f7da8a95cf24a4526a
mrhiraru <mrhiraru@gmail.com>
2025-03-08 05:46:26 +0800

    2

--- doctor/manage-appointment.php
+++ doctor/manage-appointment.php
@@ -409,7 +409,7 @@ $formattedDates = implode(', ', $fullDates);
                     available_time(dateStr, doctor_id, startTime, endTime);
                     set_value(null);
                 },
-                defaultDate: defaultAppointmentDate
+                minDate: defaultAppointmentDate
             });
 
             flatpickr("#appointment_time", {
