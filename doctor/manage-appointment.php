8cfd8b50d6fb387a4c93117cf0b50af1d7fced8b
mrhiraru <mrhiraru@gmail.com>
2025-03-08 05:23:40 +0800

    232

--- doctor/manage-appointment.php
+++ doctor/manage-appointment.php
@@ -347,7 +347,7 @@ $formattedDates = implode(', ', $fullDates);
         var endTime = subtractOneHour("<?= $_SESSION['end_wt'] ?>");
         var rawendTime = "<?= $_SESSION['end_wt'] ?>";
         //var request_btn = document.getElementById('request');
-        var full_dates = "<?= $formattedDates ?>";
+        var full_dates = <?= $formattedDates ?>;
         console.log(full_dates);
         var doctor_id = "<?= $_SESSION['doctor_id'] ?>";
 
