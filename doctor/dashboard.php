0f224b62798ee6872058685568291bd36318160c
Frankii <franklinoliveros2003@gmail.com>
2025-03-08 15:17:09 +0800

    Update dashboard.php

--- doctor/dashboard.php
+++ doctor/dashboard.php
@@ -158,10 +158,10 @@ include '../includes/head.php';
                                                 ?>
                                             </div>
                                             <div class="d-flex justify-content-start">
-                                                <a href="./patient-files?account_id<? $nextPatient['account_id'] ?>" class="btn btn-primary me-3 d-flex align-items-center text-light">
+                                                <a href="./patient-files?account_id=<?php echo $nextPatient['account_id']; ?>" class="btn btn-primary me-3 d-flex align-items-center text-light">
                                                     <i class="bx bx-file me-2"></i>Document
                                                 </a>
-                                                <a href="./chats" class="btn btn-outline-primary me-3 d-flex align-items-center">
+                                                <a href="./chats?account_id=<?php echo $nextPatient['account_id']; ?>" class="btn btn-outline-primary me-3 d-flex align-items-center">
                                                     <i class="bx bx-chat me-2"></i>Chat
                                                 </a>
                                             </div>
