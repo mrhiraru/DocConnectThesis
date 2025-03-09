e0240a44ebb14dace67e79ec52fca2fa236542ec
mrhiraru <mrhiraru@gmail.com>
2025-03-09 23:13:37 +0800

    232

--- moderator/dashboard.php
+++ moderator/dashboard.php
@@ -3,7 +3,7 @@ session_start();
 
 if (isset($_SESSION['verification_status']) && $_SESSION['verification_status'] != 'Verified') {
   header('location: ../user/verification.php');
-} else if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] != 0) {
+} else if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] != 3) {
   header('location: ./login.php');
 }
 
