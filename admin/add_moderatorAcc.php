90f75cde5843916c9a065fc4b21b9409144982bd
mrhiraru <mrhiraru@gmail.com>
2025-03-09 22:33:32 +0800

    2

--- admin/add_moderatorAcc.php
+++ admin/add_moderatorAcc.php
@@ -78,47 +78,57 @@ function getCurrentPage()
       <div class="col-2"></div>
 
       <div class="col-12 col-md-8">
-        <form action="" method="post">
-          <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" name="username" required placeholder="Enter your username" value="<?= isset($_POST['username']) ? $_POST['username'] : '' ?>">
-            <?php
-            $new_account = new Account();
-            if (isset($_POST['username'])) {
-              $new_account->email = htmlentities($_POST['username']);
-            } else {
-              $new_account->email = '';
-            }
-            if ($new_account->is_email_exist() && $_POST['username'] && $success !== 'success') {
-            ?>
-              <p class="text-dark m-0 ps-2">Username already exists.</p>
-            <?php
-            }
-            ?>
+        <form method="post" action="">
+          <div class="border shadow p-3 mb-5 bg-body rounded">
+            <h3 class="text-center">Add Moderator</h3>
+
+            <hr class="my-3 mx-4">
+
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" name="username" required placeholder="Enter your username" value="<?= isset($_POST['username']) ? $_POST['username'] : '' ?>">
+              <?php
+              $new_account = new Account();
+              if (isset($_POST['username'])) {
+                $new_account->email = htmlentities($_POST['username']);
+              } else {
+                $new_account->email = '';
+              }
+              if ($new_account->is_email_exist() && $_POST['username'] && $success !== 'success') {
+              ?>
+                <p class="text-dark m-0 ps-2">Username already exists.</p>
+              <?php
+              }
+              ?>
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password" required placeholder="Enter your password" value="<?= isset($_POST['password']) ? $_POST['password'] : '' ?>">
+              <?php
+              if (isset($_POST['password']) && validate_password($_POST['password']) !== "success") {
+              ?>
+                <p class="text-dark m-0 ps-2"><?= validate_password($_POST['password']) ?></p>
+              <?php
+              }
+              ?>
+            </div>
+            <div class="mb-3">
+              <label for="confirm-password" class="form-label">Confirm Password</label>
+              <input type="password" class="form-control" id="confirm-password" name="confirm-password" required placeholder="Confirm your password" value="<?= isset($_POST['confirm-password']) ? $_POST['confirm-password'] : '' ?>">
+              <?php
+              if (isset($_POST['password']) && isset($_POST['confirm-password']) && !validate_cpw($_POST['password'], $_POST['confirm-password'])) {
+              ?>
+                <p class="text-dark m-0 ps-2">Password did not match.</p>
+              <?php
+              }
+              ?>
+            </div>
+            <input type="submit" class="btn btn-primary text-light w-100" name="signup" value="Sign Up">
+            <div class="d-flex justify-content-end mt-3">
+              <a href="./moderatorsAcc" class="btn btn-secondary me-2 link-light">Cancel</a>
+              <button type="submit" class="btn btn-primary link-light" name="add">Add</button>
+            </div>
           </div>
-          <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" required placeholder="Enter your password" value="<?= isset($_POST['password']) ? $_POST['password'] : '' ?>">
-            <?php
-            if (isset($_POST['password']) && validate_password($_POST['password']) !== "success") {
-            ?>
-              <p class="text-dark m-0 ps-2"><?= validate_password($_POST['password']) ?></p>
-            <?php
-            }
-            ?>
-          </div>
-          <div class="mb-3">
-            <label for="confirm-password" class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" id="confirm-password" name="confirm-password" required placeholder="Confirm your password" value="<?= isset($_POST['confirm-password']) ? $_POST['confirm-password'] : '' ?>">
-            <?php
-            if (isset($_POST['password']) && isset($_POST['confirm-password']) && !validate_cpw($_POST['password'], $_POST['confirm-password'])) {
-            ?>
-              <p class="text-dark m-0 ps-2">Password did not match.</p>
-            <?php
-            }
-            ?>
-          </div>
-          <input type="submit" class="btn btn-primary text-light w-100" name="signup" value="Sign Up">
         </form>
 
       </div>
