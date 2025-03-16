5a2a2d44e63d243e3cadb8d9c2df7968264f033b
Frankii <franklinoliveros2003@gmail.com>
2025-03-16 12:14:26 +0800

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,36 +1,30 @@
-<footer class="py-4 pt-md-5 border-top shadow-lg">
-  <section class="contact-area" id="contact">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-6">
-          <div class="contact-content text-center">
-            <div class="d-flex justify-content-center m-0">
-              <a href="./index.php" class="d-flex align-items-center text-dark text-decoration-none mb-3">
-                <img src="../assets/images/logo.png" alt="Logo" style="height: 60px;">
-                <h1 class="fs-1 link-primary m-0 d-name">Doc<span class="link-dark mx-0">Connect</span></h1>
-              </a>
-            </div>
-            <p class="lead">DocConnect offers free telecommunication health services to all university students. Our services include virtual consultations with licensed healthcare professionals, mental health support, and access to a variety of health resources.</p>
-            <p class="lead">Stay healthy and take advantage of this convenient way to receive care. Simply contact us through our hotline or schedule an appointment online.</p>
-            <hr class="my-4">
-            <h6>Normal Road, Baliwasan, 7000 Zamboanga City</h6>
-            <h6>+63 123 456 7890<span> | </span>+63 123 456 7890</h6>
-            <div class="contact-social mt-4">
-              <ul class="list-inline">
-                <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
+<footer class="footer-mini py-3 border-top shadow-lg">
+  <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center text-center">
+      <div class="col-lg-8">
+        <div class="footer-content">
+          <a href="./index.php" class="d-flex align-items-center justify-content-center text-dark text-decoration-none mb-2">
+            <img src="../assets/images/logo.png" alt="Logo" style="height: 40px;">
+            <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark mx-0">Connect</span></h1>
+          </a>
+          <p class="small mb-2">DocConnect offers free telecommunication health services to all university students. Stay healthy and take advantage of this convenient way to receive care.</p>
+          <h6 class="small mb-2">Normal Road, Baliwasan, 7000 Zamboanga City</h6>
+          <h6 class="small mb-3">+63 123 456 7890<span> | </span>+63 123 456 7890</h6>
+          <div class="contact-social">
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
                   <i class='bx bxl-facebook'></i>
                 </a></li>
-                <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
-                  <i class='bx bxl-gmail' ></i>
+              <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
+                  <i class='bx bxl-gmail'></i>
                 </a></li>
-                <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
-                  <i class='bx bxl-messenger' ></i>
-                </a></li>
-              </ul>
-            </div>
+              <!-- <li class="list-inline-item"><a class="hover-target btn btn-outline-secondary rounded-circle" href="#">
+                  <i class='bx bxl-messenger'></i>
+                </a></li> -->
+            </ul>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </footer>
