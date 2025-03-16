0501318d02a40e5874a8273e0cb4f05a9311a9ad
mrhiraru <mrhiraru@gmail.com>
2025-03-16 13:36:33 +0800

    232

--- js/viewHideTogglePassword.js
+++ js/viewHideTogglePassword.js
@@ -14,24 +14,24 @@ document.querySelectorAll('.toggle-password').forEach(icon => {
 });
 
 // --------phone no input--------
-document.getElementById('contact').addEventListener('input', function() {
-  const prefix = "+63 ";
-  if (!this.value.startsWith(prefix)) {
-    this.value = prefix;
-  }
+// document.getElementById('contact').addEventListener('input', function() {
+//   const prefix = "+63 ";
+//   if (!this.value.startsWith(prefix)) {
+//     this.value = prefix;
+//   }
 
-  const rawDigits = this.value.slice(prefix.length).replace(/\D/g, '');
-  let formatted = "";
+//   const rawDigits = this.value.slice(prefix.length).replace(/\D/g, '');
+//   let formatted = "";
 
-  if (rawDigits.length > 0) {
-    formatted += rawDigits.slice(0, 3);
-  }
-  if (rawDigits.length > 3) {
-    formatted += " " + rawDigits.slice(3, 6);
-  }
-  if (rawDigits.length > 6) {
-    formatted += " " + rawDigits.slice(6, 10);
-  }
+//   if (rawDigits.length > 0) {
+//     formatted += rawDigits.slice(0, 3);
+//   }
+//   if (rawDigits.length > 3) {
+//     formatted += " " + rawDigits.slice(3, 6);
+//   }
+//   if (rawDigits.length > 6) {
+//     formatted += " " + rawDigits.slice(6, 10);
+//   }
 
-  this.value =prefix + formatted;
-});
+//   this.value =prefix + formatted;
+// });
