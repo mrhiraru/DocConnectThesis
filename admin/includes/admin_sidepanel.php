06deff316743667aa6464f88d90d7f902f9acd2b
Frankii <franklinoliveros2003@gmail.com>
2025-03-23 23:48:56 +0800

    Update admin_sidepanel.php

--- admin/includes/admin_sidepanel.php
+++ admin/includes/admin_sidepanel.php
@@ -2,7 +2,7 @@
   <ul class="nav nav-pills flex-column mb-auto">
     <li>
       <a href="./dashboard" class="nav-link <?php echo getCurrentPage() == 'dashboard.php' ? 'active' : 'link-dark'; ?>" aria-current="page">
-        <i class='bx bxs-dashboard me-2' ></i>
+        <i class='bx bxs-dashboard me-2'></i>
         Dashboard
       </a>
     </li>
@@ -14,28 +14,28 @@
     </li>
     <li>
       <a href="./campus" class="nav-link <?php echo getCurrentPage() == 'campus.php' ? 'active' : 'link-dark'; ?>">
-        <i class='bx bx-buildings me-2' ></i>
+        <i class='bx bx-buildings me-2'></i>
         Campuses
       </a>
     </li>
     <li>
       <a href="./appointment" class="nav-link <?php echo getCurrentPage() == 'appointment.php' ? 'active' : 'link-dark'; ?>">
-        <i class='bx bx-calendar me-2' ></i>
+        <i class='bx bx-calendar me-2'></i>
         Appointment
       </a>
     </li>
     <li>
       <a href="./analytics" class="nav-link <?php echo getCurrentPage() == 'analytics.php' ? 'active' : 'link-dark'; ?>">
-        <i class='bx bx-chart me-2' ></i>
+        <i class='bx bx-chart me-2'></i>
         Analytics & Reports
       </a>
     </li>
-    <!-- <li>
+    <li>
       <a href="./userPage" class="nav-link <?php echo getCurrentPage() == 'userPage.php' ? 'active' : 'link-dark'; ?>">
         <i class='bx bx-user-circle me-2'></i>
         User Page
       </a>
-    </li> -->
+    </li>
     <!-- <li>
       <a href="./staffs" class="nav-link <?php echo getCurrentPage() == 'staffs.php' ? 'active' : 'link-dark'; ?>">
         <i class='bx bx-group me-2' ></i>
@@ -44,7 +44,7 @@
     </li> -->
     <li>
       <a href="./privacyPolicy" class="nav-link <?php echo getCurrentPage() == 'settings.php' ? 'active' : 'link-dark'; ?>">
-        <i class='bx bx-cog me-2' ></i>
+        <i class='bx bx-cog me-2'></i>
         Settings
       </a>
     </li>
