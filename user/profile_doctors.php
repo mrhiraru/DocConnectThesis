83b1085711f2336a3e04f8b9ca024b01b4cb9ca4
mrhiraru <mrhiraru@gmail.com>
2025-03-24 15:19:39 +0800

    232

--- user/profile_doctors.php
+++ user/profile_doctors.php
@@ -44,71 +44,49 @@ include '../includes/head.php';
                         <div class="card-body">
                             <h5 class="card-title mb-2 text-green">Appointment List</h5>
                             <hr>
-                            <div class="table-responsive">
-                                <table class="table table-striped" id="eventsTable">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Doctor</th>
-                                            <th>Date & Time</th>
-                                            <th>Status</th>
-                                            <th class="text-center">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $appointment_class = new Appointment();
-                                        $appointmentArray = $appointment_class->get_patient_appointment_user($_SESSION['patient_id']);
-                                        $counter = 1;
-                                        if (empty(!$appointmentArray)) {
-                                            foreach ($appointmentArray as $item) {
-                                        ?>
-                                                <tr>
-                                                    <td><?= $counter ?></td>
-                                                    <td><?= $item['doctor_name'] ?></td>
-                                                    <td><?= date("l, M d, Y", strtotime($item['appointment_date'])) . " " . date("g:i A", strtotime($item['appointment_time'])) ?></td>
-                                                    <td><?= $item['appointment_status'] ?></td>
-                                                    <td class="text-center">
-                                                        <?php
-                                                        if ($item['appointment_status'] == 'Incoming') {
-                                                        ?>
-                                                            <a href="./appointment-view.php?account_id=<?= $_SESSION['account_id'] ?>&appointment_id=<?= $item['appointment_id'] ?>" class="btn btn-success btn-sm text-light"><i class='bx bx-play-circle me-1'></i>View</a>
-
-                                                        <?php
-                                                        } else if ($item['appointment_status'] == 'Ongoing') {
-                                                        ?>
-                                                            <a href="./appointment-view.php?account_id=<?= $_SESSION['account_id'] ?>&appointment_id=<?= $item['appointment_id'] ?>" class="btn btn-success btn-sm text-light"><i class='bx bx-play-circle me-1'></i>View</a>
-                                                        <?php
-                                                        } else if ($item['appointment_status'] == 'Pending') {
-                                                        ?>
-
-                                                        <?php
-                                                        } else if ($item['appointment_status'] == 'Completed') {
-                                                        ?>
-                                                            <a href="./appointment-view.php?account_id=<?= $_SESSION['account_id'] ?>&appointment_id=<?= $item['appointment_id'] ?>" class="btn btn-info btn-sm text-light"><i class='bx bx-file-blank me-1'></i>Result</a>
-                                                        <?php
-                                                        } else if ($item['appointment_status'] == 'Cancelled') {
-                                                        ?>
-
-                                                        <?php
-                                                        }
-                                                        ?>
-                                                    </td>
-                                                </tr>
-                                            <?php
-                                                $counter++;
-                                            }
-                                        } else {
-                                            ?>
-                                            <tr>
-                                                <td colspan="5" class="text-center">No Appointments</td>
-                                            </tr>
-                                        <?php
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <input type="text" id="search" class="form-control w-50 border-black" placeholder="Search by name">
+                                <div>
+                                    <i class='bx bx-list-ul toggle-view p-2 rounded border border-black' data-view="list" style="cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='lightgray'" onmouseout="this.style.backgroundColor='transparent'"></i>
+                                    <i class='bx bx-grid-alt toggle-view p-2 rounded border border-black' data-view="grid" style="cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='lightgray'" onmouseout="this.style.backgroundColor='transparent'"></i>
+                                </div>
                             </div>
+                            <div id="patient-container" class="mt-4 mb-3 row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                                <?php
+                                // $pateint = new Patient();
+                                // $patientArray = $pateint->get_patients($_SESSION['doctor_id']);
+
+                                // foreach ($patientArray as $item) {
+
+                                ?>
+                                    <div class="col patient-card p-1" data-name="<?= strtolower($name[0]) ?>">
+                                        <div class="card">
+                                            <a class="card-header fs-5" href="./patient-view?account_id=<?= $item['account_id']  ?>">
+                                                <p class="text-dark m-0">
+                                                    <?= $item['patient_name'] ?>
+                                                    <img src="<?php if (isset($item['account_image'])) {
+                                                                    echo "../assets/images/" . $item['account_image'];
+                                                                } else {
+                                                                    echo "../assets/images/default_profile.png";
+                                                                } ?>" alt="" height="32" width="32" class="float-end rounded-circle border border-primary">
+                                                </p>
+                                            </a>
+                                            <div class="card-body d-flex flex-column">
+                                                <div>
+                                                    <a href="./chats.php?account_id=<?= $item['account_id']  ?>"><i class='bx bx-chat'></i> Chat</a>
+                                                </div>
+                                                <div>
+                                                    <a href="./patient-appointment?account_id=<?= $item['account_id']  ?>"><i class='bx bx-calendar'></i>Appointments</a>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                                // }
+                                ?>
+                            </div>
+                            </main>
                         </div>
                     </div>
                 </div>
