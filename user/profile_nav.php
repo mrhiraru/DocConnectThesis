8ec4afc3496e187ee197a09f401e5faa13b0d27f
mrhiraru <mrhiraru@gmail.com>
2025-03-24 15:15:09 +0800

    2

--- user/profile_nav.php
+++ user/profile_nav.php
@@ -3,7 +3,7 @@
     <nav aria-label="breadcrumb" class="bg-body-tertiary border-1 border rounded-3 p-3 mb-4">
       <ol class="breadcrumb mb-0">
         <li class="breadcrumb-item <?= $general ?>" aria-current="<?= $aGeneral ?>"><a href="./profile_general" class="<?= $cGeneral ?>">General</a></li>
-        <li class="breadcrumb-item <?= $appointment ?>" aria-current="<?= $aAppointment ?>"><a href="./profile_appointment" class="<?= $cAppointment ?>">Appointment</a></li>
+        <li class="breadcrumb-item <?= $appointment ?>" aria-current="<?= $aAppointment ?>"><a href="./profile_doctors" class="<?= $cAppointment ?>">Appointment</a></li>
         <li class="breadcrumb-item <?= $doctors ?>" aria-current="<?= $aDoctors ?>"><a href="./profile_appointment" class="<?= $cDoctors ?>">My Doctors</a></li>
         <li class="breadcrumb-item <?= $setting ?>" aria-current="<?= $aSetting ?>"><a href="./profile_settings" class="<?= $cSetting ?>">Settings</a></li>
         <li class="breadcrumb-item <?= $privacy ?>" aria-current="<?= $aPrivacy ?>"><a href="./profile_privacy" class="<?= $cPrivacy ?>">Privacy</a></li>
