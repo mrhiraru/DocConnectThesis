45a3bfd57533ecdacf531ee40644bde67ee7269e
mrhiraru <mrhiraru@gmail.com>
2025-03-28 05:48:07 +0800

    2323

--- tools/pdfmaker.php
+++ tools/pdfmaker.php
@@ -1,6 +1,6 @@
 <script type="module">
-    import pdfMake from "../node_modules/pdfmake/build/pdfmake.js";
-    import pdfFonts from "../node_modules/pdfmake/build/vfs_fonts.js";
+    import pdfMake from "../node_modules/pdfmake/build/pdfmake.js"; 
+    import * as pdfFonts from "../node_modules/pdfmake/build/vfs_fonts.js";
 
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
