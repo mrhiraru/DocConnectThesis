89b0851f9309e17cf9304d3f1b63fb6c22a86f13
mrhiraru <mrhiraru@gmail.com>
2025-03-28 06:45:39 +0800

    232

--- doctor/patient-results.php
+++ doctor/patient-results.php
@@ -51,22 +51,7 @@ include '../includes/head.php';
                                 <label for="name" class="form-label mb-1">Patient Name:</label>
                                 <input id="name" class="form-control bg-light" value="<?= $item['patient_name'] ?>" readonly>
                             </div>
-                            <div class="col-6 mb-2">
-                                <label for="birthdate" class="form-label mb-1">Birthdate:</label>
-                                <input id="birthdate" class="form-control bg-light" value="<?= date('F d, Y', strtotime($item['birthdate'])) ?>" readonly>
-                            </div>
-                            <div class="col-6 mb-2">
-                                <label for="gender" class="form-label mb-1">Gender:</label>
-                                <input id="gender" class="form-control bg-light" value="<?= $item['gender'] ?>" readonly>
-                            </div>
-                            <div class="col-6 mb-2">
-                                <label for="email" class="form-label mb-1">Email:</label>
-                                <input id="email" class="form-control bg-light" value="<?= $item['email'] ?>" readonly>
-                            </div>
-                            <div class="col-6 mb-2">
-                                <label for="contact" class="form-label mb-1">Contact:</label>
-                                <input id="contact" class="form-control bg-light" value="<?= $item['contact'] ?>" readonly>
-                            </div>
+                            
                         </div>
                     <?php
                     }
