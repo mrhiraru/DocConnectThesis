8c1774d66f975a838390e82e9469420c83319da8
mrhiraru <mrhiraru@gmail.com>
2025-03-28 06:53:22 +0800

    2323

--- doctor/patient-results.php
+++ doctor/patient-results.php
@@ -59,6 +59,82 @@ include '../includes/head.php';
                                     <label for="reason" class="form-label mb-1">Reason:</label>
                                     <textarea id="reason" rows="2" cols="50" class="form-control bg-light" readonly><?= $item['reason'] ?></textarea>
                                 </div>
+                                <div class="col-12 mb-2">
+                                    <label for="complaint" class="form-label mb-1">Chief Complaint:</label>
+                                    <textarea id="complaint" name="complaint" rows="2" cols="50" class="form-control bg-light" required readonly><?= $item['complaint'] ?></textarea>
+                                </div>
+                                <?php
+                                if (isset($item['medcon_history'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="medcon" class="form-label mb-1">Medical History:</label>
+                                        <textarea id="medcon" name="medcon" rows="2" cols="50" class="form-control bg-light" placeholder="If yes, please specify" required readonly><?= $item['medcon_history'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                if (isset($item['allergy'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="allergy" class="form-label mb-1">Allergies:</label>
+                                        <textarea id="allergy" name="allergy" rows="2" cols="50" class="form-control bg-light" placeholder="If yes, please specify" required readonly><?= $item['allergy'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                if (isset($item['medication'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="medication" class="form-label mb-1">Medication:</label>
+                                        <textarea id="medication" name="medication" rows="2" cols="50" class="form-control bg-light" placeholder="If yes, please specify" required readonly><?= $item['medication'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                                <p class="m-0 p-0 fs-6 text-secondary mb-2">Objective Information</p>
+                                <div class="col-12 mb-2">
+                                    <label for="observation" class="form-label mb-1">Doctor's Observation:</label>
+                                    <textarea id="observation" name="observation" rows="2" cols="50" class="form-control bg-light" required readonly><?= $item['observation'] ?></textarea>
+                                </div>
+                                <p class="m-0 p-0 fs-6 text-secondary mb-2">Assessment and Plan</p>
+                                <?php
+                                if (isset($item['diagnosis'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="diagnosis_text" class="form-label mb-1">Medical Condition:</label>
+                                        <textarea id="diagnosis_text" name="diagnosis_text" rows="2" cols="50" class="form-control bg-light" required readonly><?= $item['diagnosis'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                                <div class="col-12 mb-3">
+                                    <label for="assessment" class="form-label">Consultation Assessment:</label>
+                                    <textarea id="assessment" name="assessment" rows="2" cols="50" class="form-control bg-light" required readonly><?= $item['assessment'] ?></textarea>
+                                </div>
+                                <?php
+                                if (isset($item['plan'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="plan" class="form-label mb-1">Treatment Plan and Recommendation:</label>
+                                        <textarea id="plan" name="plan" rows="2" cols="50" class="form-control bg-light" placeholder="If yes, please specify" required readonly><?= $item['plan'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                if (isset($item['prescription'])) {
+                                ?>
+                                    <div class="col-12 mb-2">
+                                        <label for="prescription" class="form-label mb-1">Prescription:</label>
+                                        <textarea id="prescription" name="prescription" rows="2" cols="50" class="form-control bg-light" placeholder="If yes, please specify" required readonly><?= $item['prescription'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                if (isset($item['comment'])) {
+                                ?>
+                                    <div class="col-12">
+                                        <label for="comment" class="form-label">Additional Comment:</label>
+                                        <textarea id="comment" name="comment" rows="2" cols="50" class="form-control bg-light" readonly><?= $item['comment'] ?></textarea>
+                                    </div>
+                                <?php
+                                }
+                                ?>
                             </div>
                         </div>
                     <?php
