e6bec040fb2f32e279d374554b30f6ebe11fffaf
Frankii <franklinoliveros2003@gmail.com>
2025-03-31 17:54:17 +0800

    userDoctors

--- admin/includes/adminUserPage_nav.php
+++ admin/includes/adminUserPage_nav.php
@@ -3,9 +3,10 @@
     <nav aria-label="breadcrumb" class="bg-body-tertiary border-1 border rounded-3 p-3 mb-4">
       <ol class="breadcrumb mb-0">
         <li class="breadcrumb-item <?= $userPage ?>" aria-current="<?= $aUserPage ?>"><a href="./userPage" class="<?= $cUserPage ?>">Home</a></li>
-        <li class="breadcrumb-item <?= $aboutUs ?>" aria-current="<?= $aAboutUs ?>"><a href="./userAboutUs" class="<?= $cAboutUs ?>">About Us</a></li>
         <li class="breadcrumb-item <?= $services ?>" aria-current="<?= $aServices ?>"><a href="./userServices" class="<?= $cServices ?>">Services</a></li>
+        <li class="breadcrumb-item <?= $doctors ?>" aria-current="<?= $aDoctors ?>"><a href="./userDoctors" class="<?= $cDoctors ?>">Our Doctors</a></li>
         <li class="breadcrumb-item <?= $contactUs ?>" aria-current="<?= $aContactUs ?>"><a href="./userContactUs" class="<?= $cContactUs ?>">Contact us</a></li>
+        <li class="breadcrumb-item <?= $aboutUs ?>" aria-current="<?= $aAboutUs ?>"><a href="./userAboutUs" class="<?= $cAboutUs ?>">About Us</a></li>
         <li class="breadcrumb-item <?= $footer ?>" aria-current="<?= $aFooter ?>"><a href="./userFooter" class="<?= $cFooter ?>">Footer</a></li>
       </ol>
     </nav>
