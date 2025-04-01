360152404e5bbcf6922a9656b3e1c6424d97b6ba
mrhiraru <mrhiraru@gmail.com>
2025-04-01 19:24:23 +0800

    23434

--- user/file_list.php
+++ user/file_list.php
@@ -61,49 +61,114 @@ include '../includes/head.php';
             <div class="row justify-content-center">
                 <div class="col-lg-10">
                     <div class="card border-0 shadow">
-                        <table class="table table-hover doctor-files">
-                            <thead>
-                                <tr>
-                                    <th>File Name</th>
-                                    <th>Description</th>
-                                    <th>Sender</th>
-                                    <th>Date Sent</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><a href="#" class="file-link">Test_Result_April_2024.pdf</a></td>
-                                    <td>Annual blood test results</td>
-                                    <td>Dr. superman</td>
-                                    <td>2024-04-15</td>
-                                    <td>
-                                        <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
-                                            data-filename="Test_Result_April_2024.pdf"
-                                            data-description="Annual blood test results"
-                                            data-sender="Dr. Smith"
-                                            data-date="2024-04-15">
-                                            <i class='bx bx-show'></i> View
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#" class="file-link">Prescription_March_2024.pdf</a></td>
-                                    <td>Medication prescription</td>
-                                    <td>Dr. btamn</td>
-                                    <td>2024-03-22</td>
-                                    <td>
-                                        <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
-                                            data-filename="Prescription_March_2024.pdf"
-                                            data-description="Medication prescription"
-                                            data-sender="Dr. Johnson"
-                                            data-date="2024-03-22">
-                                            <i class='bx bx-show'></i> View
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="mb-5">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h6 class="text-dark fw-semibold mb-0">Doctor Uploads</h6>
+                                <div class="search-box">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control doctor-search" placeholder="Search doctor files...">
+                                    </div>
+                                </div>
+                            </div>
+                            <table class="table table-hover doctor-files">
+                                <thead>
+                                    <tr>
+                                        <th>File Name</th>
+                                        <th>Description</th>
+                                        <th>Sender</th>
+                                        <th>Date Sent</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><a href="#" class="file-link">Test_Result_April_2024.pdf</a></td>
+                                        <td>Annual blood test results</td>
+                                        <td>Dr. superman</td>
+                                        <td>2024-04-15</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
+                                                data-filename="Test_Result_April_2024.pdf"
+                                                data-description="Annual blood test results"
+                                                data-sender="Dr. Smith"
+                                                data-date="2024-04-15">
+                                                <i class='bx bx-show'></i> View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><a href="#" class="file-link">Prescription_March_2024.pdf</a></td>
+                                        <td>Medication prescription</td>
+                                        <td>Dr. btamn</td>
+                                        <td>2024-03-22</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
+                                                data-filename="Prescription_March_2024.pdf"
+                                                data-description="Medication prescription"
+                                                data-sender="Dr. Johnson"
+                                                data-date="2024-03-22">
+                                                <i class='bx bx-show'></i> View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <!-- Patient Uploads Table -->
+                        <div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h6 class="text-dark fw-semibold mb-0">Patient Uploads</h6>
+                                <div class="search-box">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control patient-search" placeholder="Search patient files...">
+                                    </div>
+                                </div>
+                            </div>
+                            <table class="table table-hover patient-files">
+                                <thead>
+                                    <tr>
+                                        <th>File Name</th>
+                                        <th>Description</th>
+                                        <th>Sender</th>
+                                        <th>Date Sent</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><a href="#" class="file-link">Patient_Record_Jane_Smith_April_2024.pdf</a></td>
+                                        <td>Medical history update</td>
+                                        <td>name</td>
+                                        <td>2024-04-10</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
+                                                data-filename="Patient_Record_Jane_Smith_April_2024.pdf"
+                                                data-description="Medical history update"
+                                                data-sender="Jane Smith"
+                                                data-date="2024-04-10">
+                                                <i class='bx bx-show'></i> View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><a href="#" class="file-link">Jane_Smith_XRay_Chest_January_2024.pdf</a></td>
+                                        <td>Chest X-ray results</td>
+                                        <td>name</td>
+                                        <td>2024-01-15</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-view" data-bs-toggle="modal" data-bs-target="#fileModal"
+                                                data-filename="Jane_Smith_XRay_Chest_January_2024.pdf"
+                                                data-description="Chest X-ray results"
+                                                data-sender="Jane Smith"
+                                                data-date="2024-01-15">
+                                                <i class='bx bx-show'></i> View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
