93e7e3a91056228a17d3da80eb7ed5cccc49efd7
mrhiraru <mrhiraru@gmail.com>
2025-04-02 00:19:21 +0800

    33

--- user/file_list.php
+++ user/file_list.php
@@ -94,7 +94,7 @@ include '../includes/head.php';
                                         foreach ($file_sent_patient as $item) {
                                     ?>
                                             <tr>
-                                                <td><a href="#" class="file-link"></a><?= $item['file_name'] ?></td>
+                                                <td><a href="../assets/files/<?= $item['file_name'] ?>" class="file-link" download><?= $item['file_name'] ?></a></td>
                                                 <td><?= $item['file_description'] ?></td>
                                                 <td><?= $item['patient_name'] ?></td>
                                                 <td><?= date("F d, Y", strtotime($item['is_created'])) ?></td>
