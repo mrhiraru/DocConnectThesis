29dd51c07fdd35606ef08e456a4a939b35da9d1f
mrhiraru <mrhiraru@gmail.com>
2025-04-08 10:46:59 +0800

    232

--- user/file_list.php
+++ user/file_list.php
@@ -32,54 +32,12 @@ include '../includes/head.php';
                     Back
                 </button>
                 <a href="./file_upload?account_id=<?= $_GET['account_id'] ?>" class="btn btn-primary text-light me-2">
-                    Upload new files
+                    Send Request
                 </a>
             </div>
             <div class="row justify-content-center">
                 <div class="col-12">
                     <div class="row gx-4">
-                        <!-- Doctor Uploads Card -->
-                        <div class="col-12 mb-4">
-                            <div class="card border-0 shadow h-100">
-                                <div class="card-body p-3">
-                                    <div class="d-flex justify-content-between align-items-center mb-3">
-                                        <h6 class="text-dark fw-semibold mb-0">Doctor Uploads</h6>
-                                    </div>
-                                    <div class="table-container" style="overflow-x: auto; max-width: 100%;">
-                                        <table class="table table-hover doctor-files w-100" id="doctorFilesTable">
-                                            <thead>
-                                                <tr>
-                                                    <th class="text-nowrap">File Name</th>
-                                                    <th class="text-nowrap">Description</th>
-                                                    <th class="text-nowrap">Uploaded By</th>
-                                                    <th class="text-nowrap">Date Uploaded</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                $file_sent_doctor = $file->show_files_doctor($_GET['account_id'], $_SESSION['account_id']);
-                                                $doctor_count = 0;
-
-                                                if (!empty($file_sent_doctor)) {
-                                                    foreach ($file_sent_doctor as $item) {
-                                                        if ($doctor_count++ >= 10) break;
-                                                ?>
-                                                        <tr>
-                                                            <td class="text-truncate" style="max-width: 150px;"><a href="../assets/files/<?= htmlspecialchars($item['file_name']) ?>" class="file-link" download><?= htmlspecialchars($item['file_name']) ?></a></td>
-                                                            <td class="text-truncate" style="max-width: 150px;"><?= htmlspecialchars($item['file_description']) ?></td>
-                                                            <td class="text-truncate" style="max-width: 120px;"><?= htmlspecialchars($item['doctor_name']) ?></td>
-                                                            <td class="text-nowrap"><?= date("F d, Y", strtotime($item['is_created'])) ?></td>
-                                                        </tr>
-                                                <?php
-                                                    }
-                                                }
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                         <!-- Patient Uploads Card -->
                         <div class="col-md-12 mb-4">
@@ -124,6 +82,51 @@ include '../includes/head.php';
                                 </div>
                             </div>
                         </div>
+
+                        <!-- Doctor Uploads Card -->
+                        <div class="col-12 mb-4">
+                            <div class="card border-0 shadow h-100">
+                                <div class="card-body p-3">
+                                    <div class="d-flex justify-content-between align-items-center mb-3">
+                                        <h6 class="text-dark fw-semibold mb-0">Doctor Uploads</h6>
+                                    </div>
+                                    <div class="table-container" style="overflow-x: auto; max-width: 100%;">
+                                        <table class="table table-hover doctor-files w-100" id="doctorFilesTable">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-nowrap">File Name</th>
+                                                    <th class="text-nowrap">Description</th>
+                                                    <th class="text-nowrap">Uploaded By</th>
+                                                    <th class="text-nowrap">Date Uploaded</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $file_sent_doctor = $file->show_files_doctor($_GET['account_id'], $_SESSION['account_id']);
+                                                $doctor_count = 0;
+
+                                                if (!empty($file_sent_doctor)) {
+                                                    foreach ($file_sent_doctor as $item) {
+                                                        if ($doctor_count++ >= 10) break;
+                                                ?>
+                                                        <tr>
+                                                            <td class="text-truncate" style="max-width: 150px;"><a href="../assets/files/<?= htmlspecialchars($item['file_name']) ?>" class="file-link" download><?= htmlspecialchars($item['file_name']) ?></a></td>
+                                                            <td class="text-truncate" style="max-width: 150px;"><?= htmlspecialchars($item['file_description']) ?></td>
+                                                            <td class="text-truncate" style="max-width: 120px;"><?= htmlspecialchars($item['doctor_name']) ?></td>
+                                                            <td class="text-nowrap"><?= date("F d, Y", strtotime($item['is_created'])) ?></td>
+                                                        </tr>
+                                                <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
