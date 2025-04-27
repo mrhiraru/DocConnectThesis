e7166ad33cd4c4a85eb331074a88170c3bddd206
Frankii <franklinoliveros2003@gmail.com>
2025-04-27 17:30:16 +0800

    Update appointment.show_doctor_info.php

--- handlers/appointment.show_doctor_info.php
+++ handlers/appointment.show_doctor_info.php
@@ -15,8 +15,8 @@ $record = $account->get_doctor_info_2($_GET['account_id']);
 <div class="col-12 col-md-7">
     <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Name: <span class="text-black" id="doctor_name"><?= $record['doctor_name'] ?></span> </p>
     <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Specialty: <span class="text-black" id="specialty"><?= $record['specialty'] ?></span> </p>
-    <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Contact: <span class="text-black" id="contact"><?= $record['contact'] ?></span> </p>
-    <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Email: <span class="text-black" id="email"><?= $record['email'] ?></span> </p>
+    <!-- <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Contact: <span class="text-black" id="contact"><?= $record['contact'] ?></span> </p>
+    <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Email: <span class="text-black" id="email"><?= $record['email'] ?></span> </p> -->
     <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Working Days: <span class="text-black" id="working_day"><?= $record['start_day'] . ' to ' . $record['end_day'] ?></span> </p>
     <p class="fs-6 fw-semibold text-dark mb-1 text-black-50">Working Time: <span class="text-black" id="working_time"><?= date("g:i A", strtotime($record['start_wt'])) . " to " . date("g:i A", strtotime($record['end_wt'])) ?></span> </p>
 </div>
