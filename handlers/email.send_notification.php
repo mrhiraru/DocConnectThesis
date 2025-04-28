5a46498d46a2d2c12c67c52518b89d2d3e14c257
mrhiraru <mrhiraru@gmail.com>
2025-04-28 16:52:26 +0800

    232

--- handlers/email.send_notification.php
+++ handlers/email.send_notification.php
@@ -1,5 +1,8 @@
 <?php
 require_once('../tools/mailer.php');
+require_once('../classes/appointment.class.php');
+
+$appointment = new Appointment();
 
 $appointment_details = $appointment->get_appointment_fulldetails($appointment->appointment_id);
 email_notification($appointment_details['appointment_date'], $appointment_details['appointment_time'], $appointment_details['doctor_email'], $appointment_details['patient_name'], $appointment_details['patient_email'], $appointment_details['doctor_name'], $appointment->appointment_link, 'confirm');
